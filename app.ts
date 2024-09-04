a28f5067e7d5daf470483b56193518b69db199e1
SanSan63kc <Reagent-163-RW-08@yandex.ru>
2024-09-04 18:38:36 +0400

    017 enum

--- app.ts
+++ app.ts
@@ -1,5 +1,33 @@
-let skills: readonly [string, number] = ["Dev", 1] 
+enum StatusCode {
+    SUCCESS = 1,
+    IN_PROCESS = 3,
+    FAILED = 10
+}
 
-//skills[1] =2
+let res = {
+    message: "Платёж успешен",
+    statusCode: StatusCode.SUCCESS
+}
 
-let skill: ReadonlyArray<string> = ["Dev", "DevOps"]
+if (res.statusCode = StatusCode.SUCCESS) {
+
+}
+
+function action(status: StatusCode) {
+
+}
+
+action(StatusCode.SUCCESS)
+action(1)
+action(3)
+
+let compute = () => {
+    return 3
+}
+
+const enum Roles {
+    ADMIN = 1,
+    USER = 2
+}
+
+let res2 = Roles.ADMIN
