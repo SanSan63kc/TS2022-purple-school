7a06af5e030b974519e225d62a3d0e9083e11237
SanSan63kc <Reagent-163-RW-08@yandex.ru>
2024-09-04 16:16:22 +0400

    016 readonly

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
-let skills: [string, number] = ["Dev", 1] 
-let id  = skills[1]
-let skillName = skills[0]
+let skills: readonly [string, number] = ["Dev", 1] 
 
-let arr: [number, string, ...boolean[]] = [1, "dsa", true, false, false]
+//skills[1] =2
+
+let skill: ReadonlyArray<string> = ["Dev", "DevOps"]
