cd6fa0e2bf4deac3f8460d66beafac09cef2fa8c
SanSan63kc <Reagent-163-RW-08@yandex.ru>
2024-09-05 15:00:42 +0400

    025 optional

--- app.ts
+++ app.ts
@@ -1,17 +1,31 @@
-/* interface User {
-    name: string,
-}
-
 interface User {
-    age: number
+    login: string,
+    password?: string
 }
 
 let user: User = {
-    name: "saa",
-    age: 33
-} */
+    login: "a@a.ru",
+    password: "1"
+}
 
-type ID  = string | number
-interface IDI {
-    ID: string | number
+function multiply(first: number, second?: number): number {
+    if (!second) {
+        return first * first
+    }
+    return first * second
+}
+
+interface UserPro {
+    login: string,
+    password?: {
+        type: "primary" | "secondary"
+    }
+}
+
+function testPass(user: UserPro){
+    let t = user.password?.type
+}
+
+function test(param? : string){
+    let t = param ?? multiply(5)
 }
