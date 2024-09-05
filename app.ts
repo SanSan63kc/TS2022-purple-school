f4120583bcf1f84321648186676a1d3871082887
SanSan63kc <Reagent-163-RW-08@yandex.ru>
2024-09-05 12:05:12 +0400

    023 interfaces

--- app.ts
+++ app.ts
@@ -1,18 +1,46 @@
-type User = {
+interface User {
     name: string,
     age: number,
     skills: string[]
+
+    log: (id: number) => string
 }
 
-type Role = {
-    id: number;
+interface Role{
+    roleId: number
 }
 
-type UserWithRole = User & Role
+interface UserWithRole extends User, Role{
+    createdAt: Date
+}
+
+type User2 = {
+    name: string,
+    age: number,
+    skills: string[]
+
+    log: (id: number) => string
+}
 
 let user: UserWithRole  = {
     name: "asd",
     age: 33, 
     skills: ["1", "2"],
-    id: 1
+    roleId: 1,
+    createdAt: new Date(),
+
+    log(id){
+        return ""
+    }
 }
+
+interface UserDict{
+    [index: number]: User
+}
+
+type UserDict2 = {
+    [index: number]: User
+}
+
+
+type ud = Record<number, User>
