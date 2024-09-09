c4b5c181ccfa00963716e2fe18e497ab090ae367
SanSan63kc <Reagent-163-RW-08@yandex.ru>
2024-09-09 15:52:27 +0400

    042 extends vs composition

--- app.js
+++ app.js
@@ -1,29 +1,18 @@
 "use strict";
-var PaymentStatus;
-(function (PaymentStatus) {
-    PaymentStatus[PaymentStatus["Holded"] = 0] = "Holded";
-    PaymentStatus[PaymentStatus["Processed"] = 1] = "Processed";
-    PaymentStatus[PaymentStatus["Reversed"] = 2] = "Reversed";
-})(PaymentStatus || (PaymentStatus = {}));
-class Payment {
-    constructor(id) {
-        this.id = id;
-        this.createdAt = new Date();
-        this.status = PaymentStatus.Holded;
-    }
-    getPaymentLifeTime() {
-        return new Date().getTime() - this.createdAt.getTime();
-    }
-    unholdPayment() {
-        if (this.status === PaymentStatus.Processed) {
-            throw new Error("Платёж не может быть возвращен");
-        }
-        this.status = PaymentStatus.Reversed;
-        this.updatedAt = new Date();
+class User {
+    constructor(name) {
+        this.name = name;
     }
 }
-let payment = new Payment(1);
-payment.unholdPayment();
-console.log(payment);
-let time = payment.getPaymentLifeTime();
-console.log(time);
+class Users extends Array {
+    searchByName(name) {
+        return this.filter(u => u.name === name);
+    }
+    toString() {
+        return this.map(u => u.name).join(", ");
+    }
+}
+let users = new Users();
+users.push(new User("saa"));
+users.push(new User("alex"));
+console.log(users.toString());
--- app.ts
+++ app.ts
@@ -1,65 +1,48 @@
-type PaymentStatus1 = "new" | "paid"
-
-class Payment {
-    id: number
-    status: PaymentStatus1 = "new"
-
-    constructor(id: number) {
-        this.id = id
-    }
-
-    pay() {
-        this.status = "paid"
-    }
-}
-
-class PersistedPayment extends Payment {
-    databaseId: number
-    paidAt: Date
-
-    constructor() {
-        let id = Math.random()
-        super(id)
-    }
-
-    save() {
-        // сохраняет базу
-    }
-
-    override pay(date?: Date) {
-        super.pay()
-        this.status = "paid"
-        if (date) {
-            this.paidAt = date
-        }
-    }
-}
-
 class User {
-    name: string = "user"
+    name: string
 
-    constructor() {
-        console.log(this.name)
+    constructor(name: string) {
+        this.name = name
     }
 }
 
-class Admin extends User {
-    name: string = "admin"
+class Users extends Array<User> {
+    searchByName(name: string) {
+        return this.filter(u => u.name === name)
+    }
 
-    constructor() {
-        super()
-        console.log(this.name)
+    override toString(): string{
+        return this.map(u=>u.name).join(", ")
     }
 }
 
-new Admin()
+let users = new Users()
+users.push(new User("saa"))
+users.push(new User("alex"))
+console.log(users.toString())
 
-new Error("")
+class UsersList{
+    users: User[]
 
-class HttpError extends Error {
-    code: number
-    constructor(m: string, code?: number) {
-        super(m)
-        this.code = code ?? 500
+    push(u: User){
+        this.users.push(u)
+    }
+}
+
+class Payment{
+    date: Date
+}
+
+class UserWithPayment extends Payment{
+    name: string
+}
+
+class UserWithPayment2 {
+    user: User
+    payment: Payment
+
+    constructor(user: User, payment: Payment){
+        this.payment = payment
+        this.user = user
     }
 }
