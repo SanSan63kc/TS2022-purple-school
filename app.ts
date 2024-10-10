6658fb11940250dfdbde092b28711342f39074f8
sansan <reagent-163-rw-08@yandex.ru>
2024-10-10 07:52:37 +0300

    065 generics classes

--- app.ts
+++ app.ts
@@ -1,21 +1,24 @@
-class Vehicle {
-    run: number
+class Res<D, E> {
+    data: D;
+    error: E;
+
+    constructor(data: D, error: E) {
+        if (data) {
+            this.data = data;
+        }
+        if (error) {
+            this.error = error;
+        }
+    }
 }
 
-function kmToMiles<T extends Vehicle>(vehicle: T): T {
-    vehicle.run = vehicle.run / 0.62
-    return vehicle
+let res = new Res<string, number>("data", 0)
+
+class HTTPResp<F> extends Res<string, number>{
+    code: number;
+    setCode(code: number){
+        this.code = code
+    }
 }
 
-class LCV extends Vehicle {
-    capacity: number
-}
-
-let vehicle = kmToMiles(new Vehicle())
-let lcv = kmToMiles(new LCV())
-
-function logId<T extends string | number, Y >(id: T, additionalData: Y): {id: T, data: Y} {
-    console.log(id)
-    console.log(additionalData)
-    return {id, data: additionalData}
-}
+let res2 = new HTTPResp("data", 0)
