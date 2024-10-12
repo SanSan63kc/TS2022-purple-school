b2cbeec90972815dd4d6ee5a394ee5a827413c23
sansan <reagent-163-rw-08@yandex.ru>
2024-10-12 14:01:23 +0300

    073 infer

--- app.ts
+++ app.ts
@@ -1,50 +1,13 @@
-let a1: number = Math.random() > 0.5 ? 1 : 0
-
-interface HTTPResponse<T extends "success" | "failed"> {
-    code: number;
-    data: T extends "success" ? string : Error;
-    /* data2: T extends "success" ? string : number */
-    /*  additionalData: string | number */
+function runTransaction(transaction: {
+    fromTo: [string, string]
+}) {
+    console.log()
 }
 
-let suc: HTTPResponse<"success"> = {
-    code: 200,
-    data: "done"
+let transaction: GetFirstArg<typeof runTransaction> = {
+    fromTo: ['1', '2'] as [string, string]
 }
 
-let err: HTTPResponse<"failed"> = {
-    code: 200,
-    data: new Error()
-}
+runTransaction(transaction)
 
-class User {
-    id: number;
-    name: string;
-}
-
-class UserPersistend extends User {
-    dbId: string
-}
-
-function getUser(id: number): User
-function getUser(dbId: string): UserPersistend
-function getUser(dbOrId: string | number): User | UserPersistend {
-    if (typeof dbOrId === "number") {
-        return new User()
-    } else {
-        return new UserPersistend
-    }
-}
-
-type UserOrUserPersistend<T extends string | number> = T extends number ? User : UserPersistend
-
-function getUser2<T extends string | number>(id: T): UserOrUserPersistend<T> {
-    if (typeof id === "number") {
-        return new User() as UserOrUserPersistend<T>
-    } else {
-        return new UserPersistend() as UserOrUserPersistend<T>
-    }
-}
-
-let res3 = getUser2(1)
-let res4 = getUser2("123")
+type GetFirstArg<T> = T extends (first: infer First, ...args: any[]) => any ? First : never
