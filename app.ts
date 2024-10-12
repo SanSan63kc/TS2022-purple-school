5dbb97f79f911f6cac8fe24fd3d95c4d860f8a9a
sansan <reagent-163-rw-08@yandex.ru>
2024-10-12 21:21:54 +0300

    074 Mapped Types

--- app.ts
+++ app.ts
@@ -1,13 +1,19 @@
-function runTransaction(transaction: {
-    fromTo: [string, string]
-}) {
-    console.log()
+type Modifier = "read" | "write" | "update"
+
+type UserRoles = {
+    customers?: Modifier,
+    projects?: Modifier,
+    adminPanel?: Modifier
 }
 
-let transaction: GetFirstArg<typeof runTransaction> = {
-    fromTo: ['1', '2'] as [string, string]
+type ModifierToAccess<Type> = {
+    + readonly [Property in keyof Type as Exclude<`canAccess${string & Property}`, "canAccessAdminPanel">]-?: boolean
 }
 
-runTransaction(transaction)
+type UserToAccess2 = ModifierToAccess<UserRoles>
 
-type GetFirstArg<T> = T extends (first: infer First, ...args: any[]) => any ? First : never
+type UserAccess = {
+    customers?: boolean,
+    projects?: boolean,
+    adminPanel?: boolean
+}
