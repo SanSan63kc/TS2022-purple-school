faa7893769a33f23666d9b264b506e6f6bd7abac
sansan <reagent-163-rw-08@yandex.ru>
2024-10-13 12:27:08 +0300

    075 Mapped Types test

--- test.ts
+++ test.ts
@@ -1,33 +1,23 @@
-interface Data {
-    group: number;
+interface IForm {
     name: string;
+    password: string
 }
 
-let data: Data[] = [
-    {group: 1, name: "a"},
-    {group: 1, name: "b"},
-    {group: 2, name: "c"},
-]
-
-interface IGroup<T> {
-    [key: string]: T[]
+let form: IForm = {
+    name: "SanSan",
+    password: "123"
 }
 
-type key = string | number | symbol
-
-function group<T extends Record<string, any>>(array: T[], key: keyof T): IGroup<T>{
-    return array.reduce<IGroup<T>>((map: IGroup<T>, item)=>{
-        let itemKey = item[key];
-        let currentEl = map[itemKey]
-        if (Array.isArray(currentEl)){
-            currentEl.push(item)
-        } else {
-            currentEl = [item]
-        }
-        map[itemKey] = currentEl
-        return map
-    }, {})
+let formValidation: Validation<IForm> = {
+    name: { isValid: true },
+    password: { isValid: false, errorMessage: "Должен быть длиннее 5 символов" }
 }
 
-let res = group<Data>(data, "group")
-console.log(res)
+type Validation<T> = {
+    [K in keyof T]: {
+        isValid: true
+    } | {
+        isValid: false
+        errorMessage: string
+    }
+}
