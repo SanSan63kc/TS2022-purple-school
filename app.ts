88570c949f8636f35d1adf09f9cfb9641de9d13f
sansan <reagent-163-rw-08@yandex.ru>
2024-10-13 17:15:21 +0300

    078 Partial, Required, Readonly

--- app.ts
+++ app.ts
@@ -1,18 +1,12 @@
-type ReadOrWrite = "read" | "write"
-type Bulk = "bulk" | ""
-
-type Access = `can${Capitalize<ReadOrWrite>}${Capitalize<Bulk>}`
-
-type ReadOrWriteBulk<T> = T extends `can${infer R}` ? R : never
-
-type T = ReadOrWriteBulk<Access>
-
-type ErrorOrSucces = "error" | "success"
-
-interface ResponseT {
-    result: `http${Capitalize<ErrorOrSucces>}`
+interface User {
+    name: string
+    age?: number
+    email: string
 }
 
-let a: ResponseT = {
-    result: "httpSuccess"
-}
+type partial = Partial<User>
+let p: partial = {}
+
+type required = Required<User>
+type readonly = Readonly<User>
+type requiredAndReadonly = Required<Readonly<User>>
