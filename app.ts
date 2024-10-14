80dbc835cf43502a353674567c7f038b59a60c63
sansan <reagent-163-rw-08@yandex.ru>
2024-10-14 20:54:35 +0300

    083 Паттерн декоратора

--- app.ts
+++ app.ts
@@ -1,17 +1,24 @@
-type A = Awaited<Promise<string>>
-type A2 = Awaited<Promise<Promise<string>>>
-
-interface IMenu {
-    name: string
-    url: string
+interface IUserService {
+    users: number
+    getUsersInDatabase(): number
 }
 
-async function getMenu(): Promise<IMenu[]>{
-    return [{name: "Аналитика", url: "analytics"}]
+class UserService implements IUserService {
+    users: 1000
+    getUsersInDatabase(): number {
+        return this.users
+    }
 }
 
-type R = Awaited<ReturnType<typeof getMenu>>
-
-async function getArray<T>(x: T): Promise<Awaited<T>[]>{
-    return [await x]
+function nullUser(obj:IUserService){
+    obj.users = 0
+    return obj
 }
+
+function logUsers(obj:IUserService){
+    console.log("Users: " + obj.users)
+    return obj
+}
+
+console.log(nullUser(new UserService()).getUsersInDatabase())
+console.log(logUsers(nullUser(new UserService())).getUsersInDatabase())
