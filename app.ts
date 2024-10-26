2190b06496488fcb443f42832c88675915077114
SanSan63kc <Reagent-163-RW-08@yandex.ru>
2024-10-26 21:45:16 +0400

    091 Декоратор параметра

--- app.ts
+++ app.ts
@@ -1,21 +1,31 @@
-class Vehicle {
-    run: number
+interface IUserService {
+    getUsersInDatabase(): number
 }
 
-function kmToMiles<T extends Vehicle>(vehicle: T): T {
-    vehicle.run = vehicle.run / 0.62
-    return vehicle
+class UserService implements IUserService {
+    private _users: number = 1000
+
+    getUsersInDatabase(): number {
+        return this._users
+    }
+
+    setUsersInDatabase(@Positive() num: number): void {
+        this._users = num
+    }
 }
 
-class LCV extends Vehicle {
-    capacity: number
+function Positive() {
+    return (
+        target: Object,
+        propertyKey: string | symbol,
+        parameterIndex: number
+    ) => {
+        console.log(target)
+        console.log(propertyKey)
+        console.log(parameterIndex)
+    }
 }
 
-let vehicle = kmToMiles(new Vehicle())
-let lcv = kmToMiles(new LCV())
-
-function logId<T extends string | number, Y >(id: T, additionalData: Y): {id: T, data: Y} {
-    console.log(id)
-    console.log(additionalData)
-    return {id, data: additionalData}
-}
+let userService = new UserService()
+userService.users = 1
+console.log(userService)
--- test.js
+++ test.js
@@ -1,17 +1,22 @@
 "use strict";
-class Logger {
-    printDate(date) {
-        this.log(date.toString());
-    }
+let data = [
+    { group: 1, name: "a" },
+    { group: 1, name: "b" },
+    { group: 2, name: "c" },
+];
+function group(array, key) {
+    return array.reduce((map, item) => {
+        let itemKey = item[key];
+        let currentEl = map[itemKey];
+        if (Array.isArray(currentEl)) {
+            currentEl.push(item);
+        }
+        else {
+            currentEl = [item];
+        }
+        map[itemKey] = currentEl;
+        return map;
+    }, {});
 }
-class MyLogger extends Logger {
-    log(message) {
-        console.log(message);
-    }
-    logWithDate(message) {
-        this.printDate(new Date());
-        this.log(message);
-    }
-}
-let logger = new MyLogger();
-logger.logWithDate("Моё сообщение");
+let res = group(data, "group");
+console.log(res);
