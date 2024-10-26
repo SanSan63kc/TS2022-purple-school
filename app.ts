4b4b4aa59c28423f9f9bb7979ef868cd2e781057
SanSan63kc <Reagent-163-RW-08@yandex.ru>
2024-10-26 22:35:39 +0400

    093 Порядок декораторов

--- app.ts
+++ app.ts
@@ -1,60 +1,25 @@
-import "reflect-metadata"
-
-let POSITIVE_METADATA_KEY = Symbol("POSITIVE_METADATA_KEY")
-
-interface IUserService {
-    getUsersInDatabase(): number
-}
-
-class UserService implements IUserService {
-    private _users: number = 1000
-
-    getUsersInDatabase(): number {
-        return this._users
-    }
-
-    @Validate()
-    setUsersInDatabase(@Positive() num: number): void {
-        this._users = num
+function Uni(name: string): any {
+    console.log(`инициализация: ${name}`)
+    return function () {
+        console.log(`вызов: ${name}`)
     }
 }
 
-function Positive() {
-    return (
-        target: Object,
-        propertyKey: string | symbol,
-        parameterIndex: number
-    ) => {
-        console.log(Reflect.getOwnMetadata("design:type", target, propertyKey))
-        console.log(Reflect.getOwnMetadata("design:paramsType", target, propertyKey))
-        console.log(Reflect.getOwnMetadata("design:returnType", target, propertyKey))
-        let existParams: number[] = Reflect.getOwnMetadata(POSITIVE_METADATA_KEY, target, propertyKey) || []
-        existParams.push(parameterIndex)
-        Reflect.defineMetadata("POSITIVE_METADATA_KEY", existParams, target, propertyKey)
+@Uni("Класс")
+class MyClass {
+    @Uni("Свойство")
+    props?: any
+
+    @Uni("Свойство static")
+    static prop2?: any
+
+    @Uni("Метод")
+    method(@Uni("Параметр метода") _: any){}
+
+    @Uni("Метод static")
+    static method2(@Uni("Параметр метода static") _: any){}
+
+    constructor(@Uni("Параметр конструктора") _: any) {
+
     }
 }
-
-function Validate() {
-    return (
-        target: Object,
-        propertyKey: string,
-        descriptor: TypedPropertyDescriptor<(...args: any[]) => any>
-    ) => {
-        let method = descriptor.value
-        descriptor.value = function (...args: any) {
-            let positiveParams: number[] = Reflect.getOwnMetadata(POSITIVE_METADATA_KEY, target, propertyKey) || []
-            if (positiveParams) {
-                for (let index of positiveParams) {
-                    if (args[index] < 0) {
-                        throw new Error("число должно быть больше нуля")
-                    }
-                }
-            }
-            return method?.apply(this, args)
-        }
-    }
-}
-
-let userService = new UserService()
-console.log(userService.setUsersInDatabase(10))
-console.log(userService.setUsersInDatabase(-1))
