6bd533a94ccd79d16dd54a83491833403cb9b4d9
SanSan63kc <Reagent-163-rw-08@yandex.ru>
2024-10-30 12:54:20 +0400

    102 Singleton

--- app2.js
+++ app2.js
@@ -1,4 +1,37 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.a = void 0;
-exports.a = 5;
+var MyMap = /** @class */ (function () {
+    function MyMap() {
+        this.map = new Map();
+    }
+    MyMap.prototype.clean = function () {
+        this.map = new Map();
+    };
+    MyMap.get = function () {
+        if (!MyMap.instance) {
+            MyMap.instance = new MyMap();
+        }
+        return MyMap.instance;
+    };
+    return MyMap;
+}());
+var Service1 = /** @class */ (function () {
+    function Service1() {
+    }
+    Service1.prototype.addMap = function (key, value) {
+        var myMap = MyMap.get();
+        myMap.map.set(key, value);
+    };
+    return Service1;
+}());
+var Service2 = /** @class */ (function () {
+    function Service2() {
+    }
+    Service2.prototype.getKeys = function (key) {
+        var myMap = MyMap.get();
+        console.log(myMap.map.get(key));
+        myMap.clean();
+        console.log(myMap.map.get(key));
+    };
+    return Service2;
+}());
+new Service1().addMap(1, "Работает");
+new Service2().getKeys(1);
--- app2.ts
+++ app2.ts
@@ -1,94 +1,36 @@
-interface IInsurance {
-    id: number;
-    status: string;
-    setVehicle(vehicle: any): void;
-    submit(): Promise<boolean>
-}
+class MyMap{
+    private static instance: MyMap
+    map: Map<number, string> = new Map()
 
-class TFInsurance implements IInsurance {
-    id: number;
-    status: string;
-    private vehicle: any;
-    setVehicle(vehicle: any): void {
-        this.vehicle = vehicle
+    private constructor(){}
+
+    clean(){
+        this.map= new Map()
     }
 
-    async submit(): Promise<boolean> {
-        let res = await fetch("",
-            {
-                method: "POST", body: JSON.stringify({ vehicle: this.vehicle })
-            })
-            let data = await res.json()
-            return data.isSuccess
+    public static get(): MyMap{
+        if (!MyMap.instance){
+            MyMap.instance = new MyMap()
+        }
+        return MyMap.instance
     }
 }
 
-class ABInsurance implements IInsurance {
-    id: number;
-    status: string;
-    private vehicle: any;
-    setVehicle(vehicle: any): void {
-        this.vehicle = vehicle
-    }
-
-    async submit(): Promise<boolean> {
-        let res = await fetch("ab",
-            {
-                method: "POST", body: JSON.stringify({ vehicle: this.vehicle })
-            })
-            let data = await res.json()
-            return data.yes
+class Service1{
+    addMap(key: number, value: string){
+        let myMap = MyMap.get()
+        myMap.map.set(key, value)
     }
 }
 
-abstract class InsuranceFactory{
-    db: any
-    abstract createInsurance(): IInsurance
-
-    saveHistory(ins: IInsurance){
-        this.db.save(ins.id, ins.status)
+class Service2{
+    getKeys(key: number){
+        let myMap = MyMap.get()
+        console.log(myMap.map.get(key))
+        myMap.clean()
+        console.log(myMap.map.get(key))
     }
 }
 
-class TFInsuranceFactory extends InsuranceFactory {
-    createInsurance(): TFInsurance{
-        return new TFInsurance()
-    }
-}
-
-class ABInsuranceFactory extends InsuranceFactory {
-    createInsurance(): ABInsurance{
-        return new ABInsurance()
-    }
-}
-
-let tfInsuranceFactory = new TFInsuranceFactory()
-let ins = tfInsuranceFactory.createInsurance()
-tfInsuranceFactory.saveHistory(ins)
-
-
-
-
-
-let INSURANCE_TYPE = {
-    tf: TFInsurance,
-    ab: ABInsurance
-}
-
-type IT = typeof INSURANCE_TYPE
-
-class InsuranceFactoryAlt{
-    db: any
-
-    createInsurance<T extends keyof IT>(type: T): IT[T]{
-        return INSURANCE_TYPE[type]
-    }
-
-    saveHistory(ins: IInsurance){
-        this.db.save(ins.id, ins.status)
-    }
-}
-
-let insuranceFactoryAlt = new InsuranceFactoryAlt()
-let ins2 = new (insuranceFactoryAlt.createInsurance("tf"))
-insuranceFactoryAlt.saveHistory(ins2)
+new Service1().addMap(1, "Работает")
+new Service2().getKeys(1)
