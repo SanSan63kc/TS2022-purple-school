2d13a566170d145f3af1c697fcc47a959e49d523
SanSan63kc <Reagent-163-RW-08@yandex.ru>
2024-11-03 20:11:34 +0400

    103 Prototype

--- app.ts
+++
@@ -1,3 +0,0 @@
-import {a} from "./app2"
-
-console.log(a)
--- app2.ts
+++ app2.ts
@@ -1,5 +1,22 @@
-export let a = 5
+interface Prototype<T>{
+    clone(): T
+}
 
-    export interface B {
-        c: number
+class UserHistory implements Prototype<UserHistory>{
+    createdAt: Date
+    constructor(public email: string, public name: string){
+        this.createdAt = new Date()
     }
+
+    clone(): UserHistory{
+        let target = new UserHistory(this.email, this.name)
+        target.createdAt = this.createdAt
+        return target
+    }
+}
+
+let user = new UserHistory("a@a.ru", "Alex")
+console.log(user)
+
+let user2 = user.clone()
+console.log(user2)
